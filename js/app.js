29d54fc096c115241122be21fb0e0731cdabec90
enisbeziau <aristo2110@gmail.com>
2025-04-23 15:39:23 +0200

    ajout de la page sciences.html, fin def.html

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const sidebar = document.getElementById('menu-latéral');
-        const handle = document.getElementById('taq');
+const handle = document.getElementById('taq');
 
-        handle.addEventListener('click', () => {
-            sidebar.classList.toggle('active');
-        });
+
+handle.addEventListener('click', () => {
+    sidebar.classList.toggle('active');
+});
